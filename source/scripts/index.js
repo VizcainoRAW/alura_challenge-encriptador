19606a3c55a75f343f7054dc852e74c251d92a35
Ivan Vizcaino <ivizcain4@cuc.edu.co>
2023-06-04 20:57:37 -0500

    apartado duncional terminado y apartado visual refinado

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,18 +1,62 @@
 let codigo_encriptado=[["e","enter"],["i", "imes"],["a","ai"],["o","ober"],["u","ufat"]]
 const input_texto =document.getElementById("input")
+const output_texto =document.getElementById("output")
+var scroll=false
 
-function encriptar(texto) {
+function encriptador(texto, selector) {
     texto = texto.toString().toLowerCase();
-    console.log(texto)
-    for (var i = 0; i < codigo_encriptado.length; i++) {
-      if (texto.includes(codigo_encriptado[i][0])) {
-        texto = texto.replaceAll(codigo_encriptado[i][0], codigo_encriptado[i][1]);
+    if (selector==true){
+      console.log("encriptando")
+      for (var i = 0; i < codigo_encriptado.length; i++) {
+        if (texto.includes(codigo_encriptado[i][0])) {
+          texto = texto.replaceAll(codigo_encriptado[i][0], codigo_encriptado[i][1]);
+        }
+      }
+    }
+    if (selector==false){
+      console.log("desencriptando")
+      for (var i = 0; i < codigo_encriptado.length; i++) {
+        if (texto.includes(codigo_encriptado[i][1])) {
+          texto = texto.replaceAll(codigo_encriptado[i][1], codigo_encriptado[i][0]);
+        }
       }
     }
     return texto;
-  }
-  
+}
 function botonEncriptar(){
-    const texto_encriptado= encriptar(input_texto.value)
-    output.value=texto_encriptado
+  const texto_encriptado= encriptador(input_texto.value,true)
+  output.value=texto_encriptado
+  escondePlaceholder()
+  scrollDown()
+}
+function botonDesencriptar(texto){
+  const texto_encriptado= encriptador(input_texto.value,false)
+  output.value=texto_encriptado
+  escondePlaceholder()
+  scrollDown()
+}
+function botonCopiar(texto){
+  navigator.clipboard.writeText(output_texto.value)
+  .then(() => {
+    console.log('Texto copiado al portapapeles')
+  })
+  .catch(err => {
+    console.error('Error al copiar al portapapeles:', err)
+  })
+}
+function escondePlaceholder(){
+  if (output_texto.value === "") {
+    output_texto.style.backgroundSize = "70%";
+  } else {
+    output_texto.style.backgroundSize = "0";
+  }
+}
+function scrollDown() {
+  if(scroll===false){
+    window.scrollTo({
+      top: document.documentElement.scrollHeight,
+      behavior: 'smooth'
+    });
+    scroll=true
+  }
 }
